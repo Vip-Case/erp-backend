0cf0917de572231a3c276b3cde37fb1633833d90
ersaayan <ersaayan@gmail.com>
2024-09-25 13:11:30 +0300

    Refactor Dockerfile and index.ts
    Update Dockerfile to include the "--watch" flag for automatic reloading of changes in the source code. Modify index.ts to change the response message from "Hello Elysia" to "Elysia is running!".

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,9 @@ import { swagger } from "@elysiajs/swagger";
 
 const app = new Elysia()
 .use(swagger())
-.get("/", () => "Hello Elysia")
+.get("/", () => "Elysia is running!")
 .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
-  
