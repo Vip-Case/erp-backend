309d6e8da2d2c25d4932d4ac8d538674d55ff95a
ersaayan <ersaayan@gmail.com>
2024-12-16 11:48:09 +0300

    Add stocktake warehouse functionality with CRUD operations and model definition

--- src/api/controllers/warehouseController.ts
+++ src/api/controllers/warehouseController.ts
@@ -3,6 +3,15 @@ import WarehouseService from '../../services/concrete/warehouseService';
 import { Context } from 'elysia';
 import { Warehouse } from '@prisma/client';
 
+export interface StocktakeWarehouse {
+    id: string;
+    warehouseId: string;
+    products: Array<{
+        stockCardId: string;
+        quantity: number;
+    }>
+}
+
 // Service Initialization
 const warehouseService = new WarehouseService();
 
@@ -80,6 +89,69 @@ export const WarehouseController = {
         } catch (error: any) {
             ctx.set.status = 500;
             return { error: "Error fetching warehouses", details: error.message };
+        }
+    },
+
+    createStocktakeWarehouse: async (ctx: Context) => {
+        const stockTakeData: StocktakeWarehouse = ctx.body as StocktakeWarehouse;
+        try {
+            const warehouse = await warehouseService.createStocktakeWarehouse(stockTakeData);
+            ctx.set.status = 200;
+            return warehouse;
+        } catch (error: any) {
+            ctx.set.status = 500;
+            return { error: "Error creating stocktake warehouse", details: error.message };
+        }
+    },
+
+    updateStocktakeWarehouse: async (ctx: Context) => {
+        const { id } = ctx.params;
+        const stockTakeData: StocktakeWarehouse = ctx.body as StocktakeWarehouse;
+        try {
+            const warehouse = await warehouseService.updateStocktakeWarehouse(id, stockTakeData);
+            ctx.set.status = 200;
+            return warehouse;
+        } catch (error: any) {
+            ctx.set.status = 500;
+            return { error: "Error updating stocktake warehouse", details: error.message };
+        }
+    },
+
+    deleteStocktakeWarehouse: async (ctx: Context) => {
+        const { id } = ctx.params;
+        try {
+            const warehouse = await warehouseService.deleteStocktakeWarehouse(id);
+            ctx.set.status = 200;
+            return warehouse;
+        } catch (error: any) {
+            ctx.set.status = 500;
+            return { error: "Error deleting stocktake warehouse", details: error.message };
+        }
+    },
+
+    getStocktakeWarehouseById: async (ctx: Context) => {
+        const { id } = ctx.params;
+        try {
+            const warehouse = await warehouseService.getStocktakeWarehouseById(id);
+            if (!warehouse) {
+                return ctx.error(404, 'Stocktake warehouse not found');
+            }
+            ctx.set.status = 200;
+            return warehouse;
+        } catch (error: any) {
+            ctx.set.status = 500;
+            return { error: "Error fetching stocktake warehouse", details: error.message };
+        }
+    },
+
+    getAllStocktakeWarehouses: async (ctx: Context) => {
+        try {
+            const warehouses = await warehouseService.getStocktakeWarehouses();
+            ctx.set.status = 200;
+            return warehouses;
+        } catch (error: any) {
+            ctx.set.status = 500;
+            return { error: "Error fetching stocktake warehouses", details: error.message };
         }
     }
 }
--- src/api/routes/v1/warehouseRoutes.ts
+++ src/api/routes/v1/warehouseRoutes.ts
@@ -3,14 +3,19 @@ import { Elysia } from 'elysia';
 import WarehouseController from '../../controllers/warehouseController';
 
 export const WarehouseRoutes = (app: Elysia) => {
-    app.group("/warehouses", (app) =>
-        app.get("/", WarehouseController.getAllWarehouses, { tags: ["Warehouses"] })
-            .post("/", WarehouseController.createWarehouse, { tags: ["Warehouses"] })
-            .get("/:id", WarehouseController.getWarehouseById, { tags: ["Warehouses"] })
-            .put("/:id", WarehouseController.updateWarehouse, { tags: ["Warehouses"] })
-            .delete("/:id", WarehouseController.deleteWarehouse, { tags: ["Warehouses"] })
-            .get("/filter", WarehouseController.getWarehousesWithFilters, { tags: ["Warehouses"] })
-    );
+  app.group("/warehouses", (app) =>
+    app.get("/", WarehouseController.getAllWarehouses, { tags: ["Warehouses"] })
+      .post("/", WarehouseController.createWarehouse, { tags: ["Warehouses"] })
+      .get("/:id", WarehouseController.getWarehouseById, { tags: ["Warehouses"] })
+      .put("/:id", WarehouseController.updateWarehouse, { tags: ["Warehouses"] })
+      .delete("/:id", WarehouseController.deleteWarehouse, { tags: ["Warehouses"] })
+      .get("/filter", WarehouseController.getWarehousesWithFilters, { tags: ["Warehouses"] })
+      .post("/stocktake", WarehouseController.createStocktakeWarehouse, { tags: ["Warehouses"] })
+      .post("/stocktake/:id", WarehouseController.updateStocktakeWarehouse, { tags: ["Warehouses"] })
+      .get("/stocktake", WarehouseController.getAllStocktakeWarehouses, { tags: ["Warehouses"] })
+      .get("/stocktake/:id", WarehouseController.getStocktakeWarehouseById, { tags: ["Warehouses"] })
+      .delete("/stocktake/:id", WarehouseController.deleteStocktakeWarehouse, { tags: ["Warehouses"] })
+  );
   return app;
 };
 
